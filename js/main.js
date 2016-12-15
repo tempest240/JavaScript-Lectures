73fe736a2b27a955ae4e3a839b3c02a211f94f16
tempest240 <tempest240@mail.ru>
2016-12-15 21:25:31 +0200

    lesson2

---
+++ js/main.js
@@ -0,0 +1,234 @@
+// // console.log("2 + 2 *2=",2 + 2 * 2); //6;
+// console.log("2+2=",2+2);//4;
+// console.log("5-3=",5-3); //2;
+// console.log("5*3=",5*3);//15;
+// console.log("5%3 =",5%3); //2;
+// var x=25;
+// console.log("-x",-x);//-25
+
+// var y = 25;
+// console.log("y=",y)//string 25;
+// console.log("typeof y=", typeof y);
+// console.log("+y =",+y);//number 25;
+// console.log("typeof y=",typeof +y);
+
+// var z='10';
+// z=+z;
+// console.log("typeofz=",typeof z);//number
+
+// console.log("5>3",5>3); // true;
+// console.log("5<3",5<3); // false;
+// console.log("3<=5",3>=5); //true;
+// console.log("3<=5",3<=5); //true;
+// console.log("5!=5",5!=5); //false;
+// console.log("5!==5",5!==5); //true;
+// console.log("null=undefiend",null==undefined); //true;
+// console.log("null==undefiend",null===undefined); //false;
+
+
+
+// =================================================================================================================
+//true  
+
+
+
+// =================================================================================================================
+
+
+
+// function test(){
+	// return "test sucess!";
+// }
+
+// console.log(false && test()&& 0 &&"ends of ands list");false; //end of ands;
+
+// console.log(true && test()&& 0 &&"ends of ands list");false; //0;
+// console.log(false 0&&6); //false;
+// console.log(false ||6);//6;
+// console.log(true || 6);//true;
+// console.log(false || "end" ||0);//end;
+// console.log("start" || true || 0);//start;
+
+// console.log("|0",!0);//true;                                                                             1
+// console.log('!""=',!"");//true;                                                                          1
+// console.log('!"true because contains text"=',!"true because contains text");//false;                     1kakoeto uebanstvo!!!zbs davai bistree 
+// console.log("!\"true because \\contains text\",!!"true because contains text\");//false;            
+// console.log("true",!true);//false;                                                                       1
+
+// var x = (3,4,5);
+// console.log(x);//5;
+
+ // var x = 10,y=5,z= x+y;
+// console.log(z);//15;
+// console.log("undefined+25",undefined +25);//NaN;
+// console.log("==NaN",NaN==NaN);//false;
+// console.log("NaN===",NaN===NaN);//false;
+// console.log("typeof NaN",typeof NaN);//number;
+// console.log("2*(2+3)",2*(2+3));//10;
+
+// var x =10;
+// x++;//10
+// console.log(x);//11;
+// console.log(x++);//11;
+
+// var y =10;
+// ++y;//11
+// console.log(y);//12;
+// console.log(++y);//12;
+
+
+// var x =10;
+// x--;//10
+// console.log(x);//9;
+// console.log(x--);//9;
+
+// var y =10;
+// --y;//9
+// console.log(y);//9;
+// console.log(--y);//8;
+
+// console.log("x>y",'x'>'y');//false;
+// console.log("x>Y",'x'>'Y');//true;
+// console.log("'tes' > 'tes'=" ,'test'>'tes');//true;
+// console.log("'tes' > 'tes'=",'test'>'zes');//false;
+// console.log("'1' >'2'=",'1','2');
+// console.log("'12'>'2'",'12'>'2');
+
+// console.log("'2'>1='",'2'>1);//true;                       zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
+// console.log("'1.0'==1",'1.0==1');//true;
+// console.log("true==1",true==1);//true;
+// console.log("'false== 0 && false=='",false == 0 && false ==);//true;
+// console.log("false ==0 ",false == 0);//true;
+
+// console.log("null==undefined"null==undefined);//true;
+// console.log(null>0);//false;
+// console.log(null==0);//false;
+// console.log(null<0);////false;
+// console.log(null<=0);//true;
+// console.log(null>=0);//true;
+
+// true ==!"",!0,false, null,!undefined, !NaN;
+// false =="",0,false,null,undefined,NaN;
+
+// if (true){
+	// console.log(true);
+	// }
+
+// if (true)
+	// console.log("1");
+
+// if (25 && 0 || "text")
+	// console.log("sucess");
+// else
+	// console.log("not sucsess":(");
+
+
+// if(25 && 0 ||) false);{
+	// console.log("Sucsess&");
+// }
+// console.log("not sucess:(");
+
+// var UserName = 325;//or "VASYA" || "PETYA";
+// if (UserName == "Vasya"){
+// alert("Hello admin"+name);
+// }
+// else if (typeof UserName=="string"){
+	// alert ("Hello user" +name);
+// }
+// else{
+	// alert("You enter not a name!!");
+// }
+
+// var x = /*if*/10>15?/*then*/"test" :/*else*/"rest";
+// var y = /*if*/10<15?/*then*/"test" :/*else*/"rest";
+// console.log(x);
+// console.log(y);
+
+// if(10>15){
+	// var x="test"
+	// }else{
+		// var x ="rest"
+	// }
+// // var x = /*if*/10>15?/*then*/"test" :/*else*/"rest";
+	// var y =10<15
+// console.log(x);//rest;
+// console.log(y);//test;
+// var z =10>15? "test" : true ? "last" : "best";//true=last false = best;
+// console.log(z);//last
+
+// var x = "25";
+// switch(x){
+	// case "text":
+		// console.log("Action 1");
+		// break ;
+	// case"lost":
+		// console.log("Action 2");
+		// brake;
+	// case true:
+		// console.log("Action 3");
+		// break;
+		// default:
+		// console.log("Default Action");
+
+// }
+
+
+// var x = '25';
+// if (typeof x =="string" && x >= 25 || x != "some text"){
+	// console.log("Action 1");
+// }
+
+
+
+// var x = [];
+// if (typeof x == "array" && x [0] === undefined){
+	// console.log("Action 1");
+// }
+// else{
+	// console.log("Nope because x=",typeof x);
+// }
+
+
+var x = [];
+if (typeof x == "array" && x [0]===undefined){
+	console.log("Action 1");
+}
+else {
+	console.log("Nope because x=", typeof x);
+}
+
+var x = [];
+if (x[]=== undefined ){
+	console.log("Action 1");
+	console.log("because any not defined entity is 'undefined'");
+
+}
+else if (y % 2){
+	console.log("Action 2");
+}
+else 
+	console.log("Action 3");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
