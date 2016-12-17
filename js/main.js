88b5fd0b002b6547a61d8ca64010f049743e353d
tempest240 <tempest240@mail.ru>
2016-12-17 13:18:41 +0200

    end of lesson///

--- js/main.js
+++ js/main.js
@@ -189,38 +189,274 @@
 // }
 
 
-var x = [];
-if (typeof x == "array" && x [0]===undefined){
-	console.log("Action 1");
+// var x = [];
+// if (typeof x == "array" && x [0]===undefined){
+	// console.log("Action 1");
+// }
+// else {
+	// console.log("Nope because x=", typeof x);
+// }
+
+// var x = [];
+// if (x[]=== undefined ){
+	// console.log("Action 1");
+	// console.log("because any not defined entity is 'undefined'");sdwdsdwdsds
+
+// }
+// else if (y % 2){
+	// console.log("Action 2");
+// }
+// else 
+	// console.log("Action 3");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//+================================================================================
+
+
+
+// var i=0;
+// while (i<10){
+	// i++;//1
+	// console.log(i);
+// }
+
+// var i = 0; //runs at list once;
+// do{
+	// i++;
+	// console.log(i);
+
+// }while (false);
+
+
+// var i = 0; //runs at list once;
+// do{
+	// i++;
+	// console.log(i);
+
+// }while (false);
+
+// for (i=0;1<10;++i){
+	// console.log(i);
+// }
+
+
+// for (var i=10;i>5;i--){
+	// console.log(i);
+// }
+
+// for (var i=10;i>0;i-=2){
+	// console.log(i);
+// }
+
+
+// var i=10;
+// for(; i>5;i--){
+	// i--;
+	// console.log(i);
+// }
+
+// var i=0;
+// while (i<100){
+	// console.log(i);
+	// i++;
+	// if(i>10){
+		// break;
+	// }
+// }
+
+// var i=0;
+// do{
+	// console.log(i);
+	// i++;
+	// if(i>10){
+		// break
+	// }
+// }while(i<100);
+
+// for (var i=0;1<100;i++){
+	// console.log(i);
+	// if (i>10){
+		// break;
+	// }
+// }
+ // var i =0; while (i<10){ 	i++; 	if(i>5 && i<8){ 	continue; 	} 	console.log(i); }while(i<10) 
+
+
+// for (var i=0;i<10;i++){
+	// if (i>5 && i<8){
+		// continue
+	// }
+	// console.log(i);
+// }
+
+// for (var i=1;i<10;i++){
+	// i>5 && <9 ? continue :null;
+	// console.log(i);//нельзя использовать если есть функция возврата'ошибка'
+// }
+
+// var i = 0;
+// outerFor:while (true){
+	// console.log('outer i=',i);
+	// while (true){
+		// i++;
+		// console.log('inner i',i);
+		// if (i>10){
+			// break outerFor;
+		// }
+	// }
+// }
+
+// var i =0;
+// outerFor:do{
+	// console.log('outer i=',i);
+	// do{
+		// i++;
+		// console.log('inner i=',i);
+		// if (i>10){
+			// break outerFor;
+		// }
+	// }while(true);
+// }while (true);
+
+
+// outerFor:for(var i=0;;){
+	// console.log('outer i=',i);
+	// for(;;){
+		// i++;
+		// console.log('inner i=',i);
+		// if(i>10){
+			// break outerFor;
+		// }
+	// }
+// }
+
+
+// outerFor: for(var i=0;i<1000;){
+	// console.log('outer i=',i);
+	// for(;;){
+		// i++;
+		// console.log('inner i=',i);
+		// if(i>10){
+			// continue outer;
+		// }
+	// }
+// }
+
+// var i=0
+// outer :while (i<30){
+	// console.log('outer i=',i);
+	// while(true)
+// {
+	// console.log('inner i=',i);
+		// i++;
+		// if(i>10){
+			// continue outer;
+		// }
+	// }
+// }
+
+
+// var i=0;
+// outer: do{
+	// console.log('outer i=',i);
+	// do{
+		// i++;
+		// console.log('inner i=',i);
+		// if (i>10){
+			// continue outer;
+		// }
+	// }while(true);
+// }while(i<30)
+
+// var object ={
+	// key1: "value 1",
+	// key2: "value 2",
+	// key3: "value 3",
+	// anyKeyName:"any data",
+	// isObject:{
+		// test:"test"
+	// },
+		// isArray:[1,2,null]
+// }
+
+// console.log(object.key1);
+// console.log(object["key2"]);
+
+// for (var key in object){
+	// console.log(key);
+	// console.log(object[key]);
+// }
+
+
+// var array = [1,2,3,"string",null];
+
+// array.someKey = "some value";//dicrease speed of work with array
+
+// //index of 1 is 0;
+// //index of "string" is 3;
+// for (var index in array){
+	// console.log(index);
+	// console.log(array[index]);
+// }
+
+//Arrays;
+// var arr = [];
+// var arr = [1,"string",null];
+// var arr = new Array();//[]
+// var arr = new Array(1,"string".null);//[1,'sting',null];
+//var arr = new Array(2);//undefined;
+
+// console.log(arr.length);
+// console.log(arr[0]);
+// console.log(arr);
+
+// arr[0] = "test";
+
+// console.log(arr[0] == "test");
+
+// arr[0] ="rest";
+
+// var arr = [1,2,3];
+// console.log(arr.length);//3;
+// console.log(arr.push(10));//4;
+// console.log(arr.length);//4;
+
+// arr.push("test") > 4 && console.log("we have a big array!");
+
+// var newLeght = arr.push("rest");
+// console.log(newLeght);
+
+// var z;
+// if (z= 8>6);{
+	// console.log("z=8>6",z);
+// } 
+// console.log("Our array=",arr);
+
+// // var arr = [1,2,3];//true;
+// var arr = null;//false;
+// console.log(Array.isArray(arr));
+
+var array = ["text",123,{},[],null];
+for (var i = 0; i < array.leght;i++){
+	var currentElement = array[i];
+	if (typeof  currentElement == "object"){
+		console.log(currentElement);
+		break;
+	}
+	console.log("iteration= ",i+1);
 }
-else {
-	console.log("Nope because x=", typeof x);
-}
-
-var x = [];
-if (x[]=== undefined ){
-	console.log("Action 1");
-	console.log("because any not defined entity is 'undefined'");sdwdsdwdsds
-
-}
-else if (y % 2){
-	console.log("Action 2");
-}
-else 
-	console.log("Action 3");
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
